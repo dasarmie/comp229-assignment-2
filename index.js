db8dcf17b7b61e68cfc3d67e674e8e246b76a65d
Diego Sarmiento <dasa1088@gmail.com>
2024-02-15 14:30:53 -0500

    Connect to Dress Store's Mongodb

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.get('/', (req, res) => {
+    res.send({"message": "Welcome to DressStore application"})
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello from Node API Server Updated")
+mongoose.connect("mongodb+srv://DiegoSarmiento:4TESTMLTg07019zg@cluster.ymcpawt.mongodb.net/DressStore?retryWrites=true&w=majority")
+.then(() => {
+    console.log("Connected to Dress Store database");
+    app.listen(8080, () => {
+        console.log('Server is running on port 8080')
+    });
+})
+.catch(() => {
+    console.log("Connection failed!");
 });
