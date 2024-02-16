9e98ec08fc45fa78dc0648ccac3f0d5462a71c7d
Diego Sarmiento <dasa1088@gmail.com>
2024-02-16 18:44:54 -0500

    Create API products delete

--- index.js
+++ index.js
@@ -51,6 +51,19 @@ app.put('/api/products/:id', async(req, res) => {
     }
 });
 
+app.delete('/api/products/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const products = await Product.findByIdAndDelete(id);
+        if (!products) {
+            res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json({message: "Product delete successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 mongoose.connect("mongodb+srv://DiegoSarmiento:4TESTMLTg07019zg@cluster.ymcpawt.mongodb.net/DressStore?retryWrites=true&w=majority")
 .then(() => {
     console.log("Connected to Dress Store database");
