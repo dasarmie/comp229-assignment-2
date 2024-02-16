b005020cd15a727a60ee78b82946ada017f6f41a
Diego Sarmiento <dasa1088@gmail.com>
2024-02-16 10:31:04 -0500

    Create API get all products and get product by id

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ app.get('/', (req, res) => {
     res.send({"message": "Welcome to DressStore application"})
 });
 
+app.get('/api/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/products/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const products = await Product.findById(id);
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 app.post('/api/products', async(req, res) => {
     try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
+        const products = await Product.create(req.body);
+        res.status(200).json(products);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
